27f1c39574729f59f7b59b1a450f934eacc5aaf3
YNestA <U201417256@hust.edu.cn>
2017-02-10 00:03:28 +0800

    ToDoList transition

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'MyBlog',
         "USER":'root',
-        'PASSWORD':'××××××',
+        'PASSWORD':'******',
         'HOST':'localhost',
         'PORT':3306,
     }
